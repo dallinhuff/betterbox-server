59ba3e4f2c8965bf7987f3345df327442b94eade
dhuff <dallinhuff@gmail.com>
2023-02-01 23:35:20 -0700

    proj structure & architecture

---
+++ src/app.ts
@@ -0,0 +1,42 @@
+import express from 'express';
+import * as http from 'http';
+
+import * as winston from 'winston';
+import * as expressWinston from 'express-winston';
+import cors from 'cors';
+import {RoutesConfig} from './route/RoutesConfig';
+import debug from 'debug';
+
+const app: express.Application = express();
+const server: http.Server = http.createServer(app);
+const port = 3000;
+const routes: Array<RoutesConfig> = [];
+const debugLog: debug.IDebugger = debug('app');
+
+app.use(express.json());
+app.use(cors());
+app.use(expressWinston.logger({
+	transports: [new winston.transports.Console()],
+	format: winston.format.combine(
+		winston.format.json(),
+		winston.format.prettyPrint(),
+		winston.format.colorize({ all: true })
+	),
+	meta: !!process.env.DEBUG // when not debugging, log requests as one-liners
+}));
+
+// push route configurations to route
+
+// simple route to make sure everything is working
+const runningMessage = `Server running at http://localhost:${port}`;
+app.get('/', (req: express.Request, res: express.Response) => {
+	res.status(200).send(runningMessage)
+});
+
+server.listen(port, () => {
+	routes.forEach((route) => {
+		debugLog(`Routes configured for ${route.getName()}`);
+	});
+
+	console.log(runningMessage);
+})
