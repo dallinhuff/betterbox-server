02dc316b1d592c29c7f4cd7c8545d8b304e0ece3
dhuff <dallinhuff@gmail.com>
2023-02-04 13:36:43 -0700

    make RoutesConfig call configureRoutes in constructor show example of UserRoutesConfig and controller interaction

---
+++ src/controller/UserController.ts
@@ -0,0 +1,34 @@
+import {Handler} from "express";
+
+export const getProfile: Handler = async (req, res) => {
+	res.status(200).send('get user');
+}
+
+export const login: Handler = async (req, res) => {
+	res.status(200).send('logged in');
+}
+
+export const logout: Handler = async (req, res) => {
+	res.status(200).send('logged out');
+}
+
+export const register: Handler = async (req, res) => {
+	res.status(200).send('registered');
+}
+
+export const update: Handler = async (req, res) => {
+	res.status(200).send('updated');
+}
+
+export const remove: Handler = async (req, res) => {
+	res.status(200).send('deleted');
+}
+
+export default {
+	getProfile,
+	login,
+	logout,
+	register,
+	update,
+	remove
+}
