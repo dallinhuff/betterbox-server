20be93a317de402274856a24dfc153b4407af119
dhuff <dallinhuff@gmail.com>
2023-02-13 15:35:03 -0700

    update UserRoutesConfig to match API documentation

--- src/route/UserRoutesConfig.ts
+++ src/route/UserRoutesConfig.ts
@@ -6,17 +6,16 @@ export class UserRoutesConfig extends RoutesConfig {
 	constructor(app: Application, name: string) {
 		super(app, name);
 	}
+
 	configureRoutes(): Application {
-		this.app.route(`/user`).post(UserController.register);
+		this.app.route('/user/register').post(UserController.register);
+		this.app.route('user/login').post(UserController.login);
 		this.app
 			.route('/user/:username')
 			.get(UserController.getProfile)
 			.patch(UserController.update)
 			.delete(UserController.remove);
-		this.app
-			.route('/user/auth')
-			.post(UserController.login)
-			.delete(UserController.logout);
+
 		return this.app;
 	}
 }
