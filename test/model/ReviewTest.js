b9b01f2e7dbeea089eae0d23a16460d92c751e8d
dhuff <dallinhuff@gmail.com>
2023-02-14 12:08:18 -0700

    add testing framework

---
+++ test/model/ReviewTest.js
@@ -0,0 +1,27 @@
+describe('Review Model', function () {
+	describe('constructor', function () {
+		it('should set an author', function () {
+			this.skip();
+		});
+		it('should set a movie id', function () {
+			this.skip();
+		});
+		it('should set a rating', function () {
+			this.skip();
+		});
+		it('should set a liked property', function () {
+			this.skip();
+		});
+		it('should set a body', function () {
+			this.skip();
+		});
+		it('should set a datetime', function () {
+			this.skip();
+		});
+	});
+	describe('copy constructor', function () {
+		it('should make a deep copy', function () {
+			this.skip();
+		});
+	});
+});
